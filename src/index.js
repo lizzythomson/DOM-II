8bee88314f0bb16ca091c5249275f6a6c4555da3
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2021-11-30 14:33:28 -0700

    added four more types of event listeners

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ firstImage.addEventListener("mouseover", function (event) {
 const brokenLink = document.querySelector("a:nth-of-type(2)");
 
 brokenLink.addEventListener("click", function (event) {
-  event.preventDefault();
   console.log("Sorry! The About Us link is not working yet...Coming soon!");
+});
+
+document.addEventListener("keydown", function (event) {
+  event.preventDefault();
+  console.log("Sorry, can't use the down arrow at this time #notaccessable");
 });
 
 const firstButton = document.querySelector(".btn:nth-of-type(1)");
@@ -25,8 +29,19 @@ firstButton.addEventListener("dblclick", function (event) {
   console.log(firstButton.style);
 });
 
-document.addEventListener("scroll", function (event) {
+document.addEventListener("scroll", function () {
   console.log("you are scrolled #exciting");
+});
+
+const lastImage = document.querySelector(".content-destination img");
+
+lastImage.addEventListener("mouseenter", function () {
+  lastImage.src =
+    "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80";
+});
+
+lastImage.addEventListener("mouseleave", function () {
+  lastImage.src = "http://localhost:9000/img/destination.jpg";
 });
 
 // Your code goes here!
